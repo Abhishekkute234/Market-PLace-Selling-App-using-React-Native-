acdee07f059fe74496d91f33b7e73135ed01562f
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-10-17 09:00:38 +0530

    adding tabes in the app

---
+++ business-directory/app/(tabs)/_layout.jsx
@@ -0,0 +1,39 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color="blue" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="explore" size={24} color="blue" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="profile" size={24} color="blue" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
